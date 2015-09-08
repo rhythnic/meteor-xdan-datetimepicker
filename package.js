6f0b1dd5fa900499e8a913061b1de0b6db720aec
Nick Baroni <nick@rhythnic.com>
2015-09-08 07:01:23 -0400

    Set version to 0.0.01

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rhythnic:xdan-datetimepicker',
-  version: '0.0.10',
+  version: '0.0.01',
   summary: 'Wrapper for xdan-datetimepicker',
   git: 'https://github.com/rhythnic/meteor-xdan-datetimepicker',
   documentation: 'README.md'
