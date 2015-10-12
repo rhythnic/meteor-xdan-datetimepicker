17cd0d5641e3f64ed279664ed65603bd518e3957
Nick Baroni <nick@rhythnic.com>
2015-10-12 09:30:55 -0400

    Added explicit jquery dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rhythnic:xdan-datetimepicker',
-  version: '0.0.10',
+  version: '0.0.11',
   summary: 'Wrapper for xdan-datetimepicker',
   git: 'https://github.com/rhythnic/meteor-xdan-datetimepicker',
   documentation: 'README.md'
@@ -8,5 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
+  api.use('jquery', '');
   api.addFiles(['xdan-datetimepicker.css', 'xdan-datetimepicker.js'], 'client');
 });
